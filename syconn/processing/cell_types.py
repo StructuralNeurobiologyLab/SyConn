fixed path issues with sign prediction

syconn/processing/cell_types.py
@@ -295,7 +295,8 @@ def save_cell_type_feats(wd):
     # predict skeleton cell type probability
     skel_ids = []
     feat_dict = {}
-    result_tuple = start_multiprocess(calc_neuron_feat, skel_paths, debug=True)
+    params = [(p, wd) for p in skel_paths]
+    result_tuple = start_multiprocess(calc_neuron_feat_star, params, debug=True)
     for feat, skel_id in result_tuple:
         skel_ids.append(skel_id)
         feat_dict[skel_id] = feat[0]
@@ -303,13 +304,17 @@ def save_cell_type_feats(wd):
     # get example neuron and write neuron feature names
     cell = load_mapped_skeleton(skel_paths[0], True, True)[0]
     cell.filename = skel_paths[0]
-    neuron = Neuron(cell)
+    neuron = Neuron(cell, wd=wd)
     _ = neuron.neuron_features
     feat_names = neuron.neuron_feature_names
     np.save(wd + '/neurons/celltype_feat_names.npy', feat_names)
 
 
-def calc_neuron_feat(path):
+def calc_neuron_feat_star(params):
+    return calc_neuron_feat(params[0], params[1])
+
+
+def calc_neuron_feat(path, wd):
     """Calculate neuron features using neuron class
 
     Parameters
@@ -325,7 +330,7 @@ def calc_neuron_feat(path):
     orig_skel_id = get_skelID_from_path(path)
     cell = load_mapped_skeleton(path, True, True)[0]
     cell.filename = path
-    neuron = Neuron(cell)
+    neuron = Neuron(cell, wd=wd)
     feats = neuron.neuron_features
     if np.any(np.isnan(feats)):
         print "Found nans in feautres of skel %s" % path, \