c8c52decdce2d4b62d9bf115b547f4c707384c30
sdorkenw <sven.dorkenwald@googlemail.com>
2017-01-30 01:52:25 +0100

    qsub helper for exporting knossos datasets to chunkdatasets

---
+++ syconn/multi_proc/QSUB_export_dense_segmentation_to_cset.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# SyConn - Synaptic connectivity inference toolkit
+#
+# Copyright (c) 2016 - now
+# Max-Planck-Institute for Medical Research, Heidelberg, Germany
+# Authors: Sven Dorkenwald, Philipp Schubert, Jörgen Kornfeld
+
+import sys
+
+import cPickle as pkl
+from syconn.utils import densedataset_helper as ddh
+
+path_storage_file = sys.argv[1]
+path_out_file = sys.argv[2]
+
+with open(path_storage_file) as f:
+    args = []
+    while True:
+        try:
+            args.append(pkl.load(f))
+        except:
+            break
+
+out = ddh.export_dense_segmentation_to_cset_thread(args)
