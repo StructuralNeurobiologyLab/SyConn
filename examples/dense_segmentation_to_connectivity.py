1f59ee9abdaf6f002309bf48e0c9b0c6c7ea8c6f
sdorkenw <sven.dorkenwald@googlemail.com>
2017-02-24 22:31:15 +0100

    current state

--- examples/dense_segmentation_to_connectivity.py
+++ examples/dense_segmentation_to_connectivity.py
@@ -68,7 +68,7 @@ else:
     cset = initialization.initialize_cset(kd_raw, main_path, [512, 512, 256])
     chunky.save_dataset(cset)
 
-# ------------------------------------------------------------ SuperVoxel Extraction
+# -------------------------------------------------------- SuperVoxel Extraction
 
 if qsub_queue or qsub_pe:
     import_batch_size = int(len(cset.chunk_dict.keys()) / 1000)
@@ -80,15 +80,7 @@ ddh.export_dense_segmentation_to_cset(cset, kd_raw, datatype=np.uint32,
                                                nb_cpus=8, pe=qsub_pe,
                                                queue=qsub_queue,
                                                batch_size=import_batch_size)
-# ch_sz = cset.chunk_size
-# xs = [ch_sz[0]*i for i in range(8, 12)]
-# ys = [ch_sz[0]*i for i in range(8, 12)]
-# zs = [ch_sz[0]*i for i in range(8, 12)]
-# chunk_list = []
-# for x in xs:
-#     for y in ys:
-#         for z in zs:
-#             chunk_list.append(cset.coord_dict[tuple([x, y, z])])
+
 
 oe.validate_chunks(cset, "dense_segmentation", ["sv"], qsub_pe=qsub_pe,
                    qsub_queue=qsub_queue)
@@ -97,7 +89,12 @@ oe.validate_chunks(cset, "dense_segmentation", ["sv"], qsub_pe=qsub_pe,
 oe.from_ids_to_objects(cset, "dense_segmentation", ["sv"],
                        debug=False, qsub_pe=qsub_pe, qsub_queue=qsub_queue)
 
-#
+
+
+
+
+
+
 # # ------------ Create hull and map objects to tracings and classify compartments
 #
 # syconn.enrich_tracings_all(main_path, qsub_pe=qsub_pe, qsub_queue=qsub_queue)
