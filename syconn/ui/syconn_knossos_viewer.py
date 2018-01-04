0891875358a87b620e13ddfd01ae79e1c4c996a6
Jörgen Kornfeld <kornfeld@neuro.mpg.de>
2018-01-04 13:39:01 +0100

    Replaced the knossos viewer pluging with the latest version supporting the backend server.

--- syconn/ui/syconn_knossos_viewer.py
+++ syconn/ui/syconn_knossos_viewer.py
@@ -1,21 +1,90 @@
-# -*- coding: utf-8 -*-
-# SyConn - Synaptic connectivity inference toolkit
-#
-# Copyright (c) 2016 - now
-# Max Planck Institute of Neurobiology, Martinsried, Germany
-# Authors: Sven Dorkenwald, Philipp Schubert, Joergen Kornfeld
-
 from PythonQt import QtGui, Qt, QtCore
 import KnossosModule
 import sys
+import requests
+import json
 sys.dont_write_bytecode = True
-from ..reps import super_segmentation_object as ss
-# make this a GUI setting / enter path to directory manually
-from ..config.global_params import wd
-syconnfs_path = wd
 
+class SyConnGateInteraction(object):
+    """
+    Query the SyConn backend server.
+    """
+    def __init__(self, server):
+        self.server = server
+        self.session = requests.Session()
+        self.ssv_from_sv_cache = dict()
+        self.svs_from_ssv = dict()
+
+    def get_ssv_mesh(self, ssv_id):
+        """
+        Returns a mesh for a given ssv_id.
+        Parameters
+        ----------
+        ssv_id
+
+        Returns
+        -------
+
+        """
+        r = self.session.get(self.server + '/ssv_mesh/{0}'.format(ssv_id))
+        return json.loads(r.content)
+
+    def get_list_of_all_ssv_ids(self):
+        """
+        Returns a list of all ssvs in the dataset
+        Returns
+        -------
+
+        """
+        r = self.session.get(self.server + '/ssv_list')
+        return json.loads(r.content)
+
+    def get_svs_of_ssv(self, ssv_id):
+        """
+        Returns a list of all svs of a given ssv.
+        Parameters
+        ----------
+        ssv_id
+
+        Returns
+        -------
+
+        """
+        if not self.svs_from_ssv.has_key(ssv_id):
+            r = self.session.get(self.server + '/svs_of_ssv/{0}'.format(ssv_id))
+            self.svs_from_ssv[ssv_id] = json.loads(r.content)
+        return self.svs_from_ssv[ssv_id]
+
+    def get_ssv_of_sv(self, sv_id):
+        """
+        Gets the ssv for a given sv.
+        Parameters
+        ----------
+        sv_id
+
+        Returns
+        -------
+
+        """
+        if not self.ssv_from_sv_cache.has_key(sv_id):
+            r = self.session.get(self.server + '/ssv_of_sv/{0}'.format(sv_id))
+            self.ssv_from_sv_cache[sv_id] = json.loads(r.content)
+        return self.ssv_from_sv_cache[sv_id]
+
+    def get_all_syn_metda_data(self):
+        """
+
+        Returns
+        -------
+
+        """
+        r = self.session.get(self.server + '/all_syn_meta')
+        return json.loads(r.content)
 
 class main_class(QtGui.QDialog):
+    """
+    KNOSSOS plugin class for the SyConn KNOSSOS viewer.
+    """
     def __init__(self, parent=KnossosModule.knossos_global_mainwindow):
         #Qt.QApplication.processEvents()
         super(main_class, self).__init__(parent, Qt.Qt.WA_DeleteOnClose)
@@ -32,22 +101,51 @@ class main_class(QtGui.QDialog):
         self.init_syconn()
         self.build_gui()
 
-    def init_syconn(self):
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.exploration_mode_callback_check)
+        self.timer.start(1000)
 
-        self.ssd = ss.SuperSegmentationDataset(syconnfs_path)
+
+    def init_syconn(self):
+        # move to config file
+        syconn_gate_server = 'http://localhost:10001'
+        self.syconn_gate = SyConnGateInteraction(syconn_gate_server)
 
         return
 
     def populate_ssv_list(self):
-        all_ssv_ids = self.get_list_of_all_ssv_ids()
+        all_ssv_ids = self.syconn_gate.get_list_of_all_ssv_ids()['ssvs']
+
         #print('list of all here')
         #print(len(all_ssv_ids))
         for ssv_id in all_ssv_ids:
             #print(ssv_id)
-            item = QtGui.QStandardItem(str(ssv_id))
+            item = QtGui.QStandardItem(str(int(ssv_id)))
             self.ssv_item_model.appendRow(item)
 
         self.ssv_selector.setModel(self.ssv_item_model)
+        return
+
+    def populate_syn_list(self):
+        all_syns = self.syconn_gate.get_all_syn_metda_data()
+
+        #print('list of all here')
+        #print(len(all_ssv_ids))
+        for syn in zip(all_syns['ssv_partner_0'], all_syns['ssv_partner_1']):
+            item = QtGui.QStandardItem(str(syn))
+            self.syn_item_model.appendRow(item)
+
+        self.syn_selector.setModel(self.syn_item_model)
+        return
+
+
+    def on_ssv_selector_changed(self, index):
+        self.ssv_selected1 = int(self.ssv_selector.model().itemData(index)[0])
+        #current, previous
+        #print('selected: ' + str(self.ssv_selector.model().itemData(index)[0]))
+        #ssv = self.get_ssv(self.ssv_selected1)
+        #self.ssv_to_knossos(ssv)
+
         return
 
     def on_ssv_selector_changed(self, index):
@@ -75,10 +173,16 @@ class main_class(QtGui.QDialog):
         self.ssv_selector.setUniformItemSizes(True) # better performance
         self.ssv_item_model = QtGui.QStandardItemModel(self.ssv_selector)
 
+        self.syn_selector = QtGui.QListView()
+        self.syn_selector.setUniformItemSizes(True) # better performance
+        self.syn_item_model = QtGui.QStandardItemModel(self.syn_selector)
+
         self.direct_ssv_id_input = QtGui.QLineEdit()
         self.direct_ssv_id_input.setValidator(QtGui.QIntValidator())
         self.direct_ssv_id_input.setMaxLength(16)
 
+        self.exploration_mode_chk_box = QtGui.QCheckBox('Exploration mode')
+        self.exploration_mode_chk_box.setChecked(False)
         #self.ssv_selection_model =
         # QtGui.QItemSelectionModel(self.ssv_select_model)
 
@@ -88,26 +192,40 @@ class main_class(QtGui.QDialog):
         #print('selection model: ' + str(self.ssv_selector.selectionModel()))
 
         self.ssv_selector.clicked.connect(self.on_ssv_selector_changed)
+        self.syn_selector.clicked.connect(self.on_ssv_selector_changed)
 
         self.populate_ssv_list()
+
+        self.populate_syn_list()
         print('Connected to SyConnFS backend')
 
         layout.addWidget(self.direct_ssv_id_input, 1, 0, 1, 2)
-        layout.addWidget(self.ssv_selector, 2, 0, 1, 2)
+        layout.addWidget(self.ssv_selector, 2, 0, 1, 1)
+        layout.addWidget(self.syn_selector, 2, 1, 1, 1)
         layout.addWidget(self.show_button, 3, 0, 1, 1)
         layout.addWidget(self.clear_knossos_view_button, 3, 1, 1, 1)
+        layout.addWidget(self.exploration_mode_chk_box, 4, 0, 1, 2)
 
         #self.ssv_select_model.itemChanged.connect(self.on_ssv_selector_changed)
         #self.selectionModel.selectionChanged.connect(self.on_ssv_selector_changed)
 
         self.show_button.clicked.connect(self.show_button_clicked)
         self.clear_knossos_view_button.clicked.connect(self.clear_knossos_view_button_clicked)
+        self.exploration_mode_chk_box.stateChanged.connect(self.exploration_mode_changed)
 
-        self.setGeometry(300, 300, 350, 300)
+        self.setGeometry(300, 300, 450, 300)
         self.setWindowTitle('SyConn Viewer v1')
         self.show()
         #self.merge_button = QtGui.QPushButton('Merge')
         #self.split_button = QtGui.QPushButton('Split')
+
+
+#        QList < quint64 > subobjectIdsOfObject(const
+ #       quint64
+  #      objId);
+
+   #     QList < quint64 > objects();
+    #    QList < quint64 > selectedObjects();
 
 
         #self.bad_button = QtGui.QPushButton('Bad SV')
@@ -118,8 +236,6 @@ class main_class(QtGui.QDialog):
         #self.undo_button = QtGui.QPushButton('Undo')
         #self.redo_button = QtGui.QPushButton('Redo')
 
-
-
         #self.skip_task_line_edit = QtGui.QLineEdit('Skip reason')
         #self.server_line_edit = QtGui.QLineEdit()
 
@@ -129,6 +245,56 @@ class main_class(QtGui.QDialog):
         #self.gui_auto_agglo_line_edit = QtGui.QLineEdit()
         #self.gui_auto_agglo_line_edit.setText('0')
 
+    def exploration_mode_changed(self):
+
+        if self.exploration_mode_chk_box.isChecked():
+            pass
+            # enable selection polling timer
+        else:
+            pass
+            # disable selection polling timer
+
+    def exploration_mode_callback_check(self):
+        if self.exploration_mode_chk_box.isChecked():
+
+            sel_seg_objs = KnossosModule.segmentation.selectedObjects()
+            sel_sv_ids = []
+            for sel_seg_obj in sel_seg_objs:
+                sel_sv_ids.append(KnossosModule.segmentation.subobjectIdsOfObject(sel_seg_obj)[0])
+
+            # get selected ssv ids
+            ssv_ids_selected = [self.syconn_gate.get_ssv_of_sv(sv_id)['ssv'] for sv_id in sel_sv_ids]
+
+            # id_changer returns -1 for a supervoxel that is unconnected, add support for single svs
+            ssv_ids_selected = [ssv_id for ssv_id in ssv_ids_selected if ssv_id != -1]
+
+            #print('ssv_ids_selected {0}'.format(ssv_ids_selected))
+
+            trees = KnossosModule.skeleton.trees()
+            ids_in_k = set([tree.tree_id() for tree in trees])
+
+            # compare with the selected segmentation objects
+            ids_selected = set(ssv_ids_selected)
+
+            # add missing ones to knossos, delete if not needed anymore
+            ids_to_add = ids_selected - ids_in_k
+            ids_to_del = ids_in_k - ids_selected
+
+            # remove segmentation objects that are not needed anymore
+            all_objects = KnossosModule.segmentation.objects()
+
+            objs_to_del = set(all_objects) - set(ids_selected)
+
+            [KnossosModule.segmentation.removeObject(obj) for obj in objs_to_del]
+
+            #print('ids to del {0} ids to add {1}'.format(ids_to_del, ids_to_add))
+
+            [KnossosModule.skeleton.delete_tree(sv_id) for sv_id in ids_to_del]
+            [self.ssv_to_knossos(ssv_id) for ssv_id in ids_to_add]
+
+        return
+
+
     def show_button_clicked(self):
         try:
             self.ssv_selected1 = int(self.direct_ssv_id_input.text)
@@ -136,9 +302,7 @@ class main_class(QtGui.QDialog):
             pass
 
         if self.ssv_selected1:
-        #ssv_id = self.ssv_selected1
-            ssv = self.get_ssv(self.ssv_selected1)
-            self.ssv_to_knossos(ssv)
+            self.ssv_to_knossos(self.ssv_selected1)
 
         return
 
@@ -148,64 +312,52 @@ class main_class(QtGui.QDialog):
         all_objects = KnossosModule.segmentation.objects()
         [KnossosModule.segmentation.removeObject(obj) for obj in all_objects]
 
-        # iterate over all tree in knossos and delete
+        # iterate over all trees in knossos and delete
         trees = KnossosModule.skeleton.trees()
         ids_in_k = set([tree.tree_id() for tree in trees])
         [KnossosModule.skeleton.delete_tree(sv_id) for sv_id in ids_in_k]
 
         return
 
-    def ssv_to_knossos(self, ssv):
+    def ssv_to_knossos(self, ssv_id):
 
-        self.clear_knossos_view_button_clicked()
+        #self.clear_knossos_view_button_clicked()
 
         # to mergelist
-        KnossosModule.segmentation.createObject(ssv.id, ssv.sv_ids[0], (1,1,1))
+        sv_ids = self.syconn_gate.get_svs_of_ssv(ssv_id)['svs']
+        sv_ids = map(int, sv_ids)
+
+        KnossosModule.segmentation.createObject(ssv_id, sv_ids[0], (1,1,1))
         #KnossosModule.segmentation.selectObject(ssv.id)
         # query object should be red
-        KnossosModule.segmentation.changeColor(ssv.id, QtGui.QColor(255, 0, 0, 255))
+        #KnossosModule.segmentation.changeColor(ssv_id, QtGui.QColor(255, 0, 0, 255))
 
         # one could cache this, not necessary to rebuild at every step
-        for sv_id in ssv.sv_ids:
+        for sv_id in sv_ids:
             try:
-                KnossosModule.segmentation.addSubobject(ssv.id, sv_id)
+                KnossosModule.segmentation.addSubobject(ssv_id, sv_id)
             except:
                 pass
 
-        KnossosModule.segmentation.selectObject(ssv.id)
+        KnossosModule.segmentation.selectObject(ssv_id)
 
         KnossosModule.segmentation.setRenderOnlySelectedObjs(True)
 
-        KnossosModule.skeleton.add_tree_mesh(1, ssv.mesh[1], [], ssv.mesh[0],
+        mesh = self.syconn_gate.get_ssv_mesh(ssv_id)
+        KnossosModule.skeleton.add_tree_mesh(ssv_id, mesh['indices'], [], mesh['vertices'],
                                              [],
                                              KnossosModule.GL_TRIANGLES,
                                              False)
-        KnossosModule.skeleton.set_tree_color(1, QtGui.QColor(255, 0, 0, 128))
+        KnossosModule.skeleton.set_tree_color(ssv_id, QtGui.QColor(255, 0, 0, 128))
 
-        KnossosModule.skeleton.add_tree_mesh(2, ssv.mi_mesh[1], [],
-                                             ssv.mi_mesh[0], [],
-                                             KnossosModule.GL_TRIANGLES,
-                                             False)
-        KnossosModule.skeleton.set_tree_color(2, QtGui.QColor(0, 0, 255, 255))
+        #KnossosModule.skeleton.add_tree_mesh(2, ssv.mi_mesh[1], [],
+        #                                     ssv.mi_mesh[0], [],
+        #                                     KnossosModule.GL_TRIANGLES,
+        #                                     False)
+        #KnossosModule.skeleton.set_tree_color(2, QtGui.QColor(0, 0, 255, 255))
 
         return
 
-    def get_list_of_all_ssv_ids(self):
-        """
-        Fetches a list of all ssvs from syconn and adds them to the list. 
-        
-        Returns
-        -------
-
-        """
-
-        return self.ssd.ssv_ids
-
-    def get_ssv(self, ssv_id):
-
-        ssv = self.ssd.get_super_segmentation_object(ssv_id)
-        ssv.load_attr_dict()
-        return ssv
 
 if __name__=='__main__':
     A = main_class()
