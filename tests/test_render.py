2dbe5a61a487e20b62a2dda5dc76fd49e023a071
atulm <atultm@neuro.mpg.de>
2019-01-03 10:34:14 +0100

    refined rendering test script

--- tests/test_render.py
+++ tests/test_render.py
@@ -5,16 +5,17 @@ import matplotlib.pyplot as plt
 from scipy.misc import imsave
 
 if __name__=='__main__':
-   # ssc=SuperSegmentationDataset('/wholebrain/scratch/areaxfs3/')   #running on cluster
-    ssc=SuperSegmentationDataset('/home/atulm/mount/wb/wholebrain/scratch/areaxfs3/')    #running on local machine
-
-    exloc=np.array([5602, 4173, 4474])
+   ssc=SuperSegmentationDataset('/wholebrain/scratch/areaxfs3/')   #running on cluster
+    #ssc=SuperSegmentationDataset('/home/atulm/mount/wb/wholebrain/scratch/areaxfs3/')    #running on local machine
     ssv = ssc.get_super_segmentation_object(29753344)
+
+    exloc=np.array([5602, 4173, 4474])*ssv.scaling
     more_exlocs = np.concate(ssv.sample_locations())
 
     views = render_sso_coords(ssv, [exloc], )
-    #imsave('/u/atultm/test_2dproj.png', views[0, 0, 0])           #running on cluster
-    imsave('/home/atultm/test_2dproj.png', views[0, 0, 0])        #running on local machine
+    imsave('/u/atultm/test_2dproj.png', views[0, 0, 0])           #running on cluster
+    #imsave('/home/atultm/test_2dproj.png', views[0, 0, 0])        #running on local machine
+    # imageio.imwrite('/u/atultm/test_2dpro1j.png', views[0, 0, 0])
 
 
 
