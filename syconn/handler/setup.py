c4f8f480d257179e08b39d19d1b4d079be9deace
mariakawula <mariakawula2911@gmail.com>
2019-02-20 17:39:20 +0100

    Merge remote-tracking branch 'origin/dense_segmentation' into obj_extr_v2
    # Conflicts:
    #	setup.py
    #	syconn/proc/meshes.py

--- syconn/handler/setup.py
+++
@@ -1,6 +0,0 @@
-# from distutils.core import setup, Extension
-# from Cython.Build import cythonize
-#
-# setup(
-#     ext_modules =cythonize(Extension("uniqueCython", sources=["uniqueCython.pyx"]))
-# )
