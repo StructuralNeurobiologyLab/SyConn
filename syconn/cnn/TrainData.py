added attributes `syn_ssv` and `syn_sign_ratio` to `SuperSegmentationObject`, updated cell type model to also utilize syn sign information, fixed usage of wrong keyword, added additional print that specifies example working directory in `example_cube/start.py`

syconn/cnn/TrainData.py
@@ -106,10 +106,10 @@ if elektronn3_avail:
             self.ctv = CelltypeViews(None, None, **kwargs)
 
         def __getitem__(self, index):
-            inp, target = self.ctv.getbatch_alternative(1, source='train' if self.train else 'valid')
+            inp, target, syn_signs = self.ctv.getbatch_alternative(1, source='train' if self.train else 'valid')
             inp, _ = self.transform(inp, None)  # Do not flip target label ^.^
             # target = np.eye(self.ctv.n_classes)[target.squeeze().astype(np.int)]  # one-hot encoding
-            return inp[0], target.squeeze().astype(np.int)  # target should just be a scalar
+            return inp[0], target.squeeze().astype(np.int), syn_signs[0].astype(np.float32)  # target should just be a scalar
 
         def __len__(self):
             """Determines epoch size(s)"""
@@ -286,7 +286,7 @@ class MultiViewData(Data):
             ssv_labels = np.array(list(self.label_dict.values()), dtype=np.uint)
             n_classes = len(np.unique(ssv_labels))
             if int(train_fraction) * len(ssv_ids) < n_classes:
-                train_fraction = 1. - float(n_classes)  / len(ssv_ids)
+                train_fraction = 1. - float(n_classes + 1) / len(ssv_ids)
                 print("Train data fraction was set to {} due to splitting restrictions "
                       "(at least one sample per class in validation set).".format(train_fraction))
             train_ids, valid_ids = train_test_split(ssv_ids, shuffle=True, random_state=random_seed, stratify=ssv_labels,
@@ -439,6 +439,7 @@ class CelltypeViews(MultiViewData):
         self.class_weights = np.array(class_weights)
         self.view_cache = {'train': None, 'valid': None, 'test': None}
         self.label_cache = {'train': None, 'valid': None, 'test': None}
+        self.syn_sign_cache = {'train': None, 'valid': None, 'test': None}
         self.sample_weights = {'train': None, 'valid': None, 'test': None}
         self.reduce_context_fact = reduce_context_fact
         self.binary_views = binary_views
@@ -517,6 +518,7 @@ class CelltypeViews(MultiViewData):
                 ssos.append(sso)
             self.view_cache[source] = [sso.load_views(view_key=self.view_key)
                                        for sso in ssos]
+            self.syn_sign_cache[source] = np.array([sso.syn_sign_ratio() for sso in ssos])
             for ii in range(len(self.view_cache[source])):
                 views = self.view_cache[source][ii]
                 views = naive_view_normalization_new(views)
@@ -530,9 +532,9 @@ class CelltypeViews(MultiViewData):
                                                                np.unique(self.label_cache[source]),
                                                                self.label_cache[source])
         ixs = np.random.choice(np.arange(len(self.view_cache[source])), batch_size, replace=False)
-        d, l = transform_celltype_data_views_alternative(
+        d, l, syn_signs = transform_celltype_data_views_alternative(
             [self.view_cache[source][ix] for ix in ixs], [self.label_cache[source][ix] for ix in ixs],
-            batch_size, self.nb_views)
+            self.syn_sign_cache[source], batch_size, self.nb_views)
         if self.reduce_context > 0:
             d = d[:, :, :, (self.reduce_context/2):(-self.reduce_context/2),
                 self.reduce_context:-self.reduce_context]
@@ -543,7 +545,7 @@ class CelltypeViews(MultiViewData):
         self.current_cache_uses += 1
         if self.raw_only:
             return d[:, :1], l
-        return tuple([d, l])
+        return tuple([d, l, syn_signs])
 
     def getbatch(self, batch_size, source='train'):
         self._reseed()
@@ -725,9 +727,10 @@ def transform_celltype_data_views(sso_views, labels, batch_size, nb_views,
     return tuple([orig_views, new_labels])
 
 
-def transform_celltype_data_views_alternative(sso_views, labels, batch_size, nb_views):
+def transform_celltype_data_views_alternative(sso_views, labels, syn_signs, batch_size, nb_views):
     orig_views = np.zeros((batch_size, 4, nb_views, 128, 256), dtype=np.float32)
     new_labels = np.zeros((batch_size, 1), dtype=np.int16)
+    new_synsigns = np.zeros((batch_size, 1), dtype=np.int16)
     cnt = 0
     # sample_fac_sv = np.max([int(nb_views / 10), 1]) # draw more SV if #views is high
     for ii, views in enumerate(sso_views):
@@ -740,6 +743,7 @@ def transform_celltype_data_views_alternative(sso_views, labels, batch_size, nb_
         orig_views[cnt:(curr_nb_samples+cnt)] = views[:, view_sampling].reshape((4, curr_nb_samples,
                                                                                  nb_views, 128, 256)).swapaxes(1, 0)
         new_labels[cnt:(curr_nb_samples+cnt)] = labels[ii]
+        new_synsigns[cnt:(curr_nb_samples+cnt)] = syn_signs[ii]
         cnt += curr_nb_samples
         if cnt == batch_size:
             break
@@ -758,8 +762,9 @@ def transform_celltype_data_views_alternative(sso_views, labels, batch_size, nb_
             random_ix = np.random.choice(np.arange(cnt), 1, replace=False)
             orig_views[cnt:(curr_nb_samples + cnt)] = orig_views[random_ix]
             new_labels[cnt:(curr_nb_samples + cnt)] = new_labels[random_ix]
+            new_synsigns[cnt:(curr_nb_samples + cnt)] = new_synsigns[random_ix]
             cnt += curr_nb_samples
-    return tuple([orig_views, new_labels])
+    return tuple([orig_views, new_labels, new_synsigns])
 
 
 def transform_celltype_data(ssos, labels, batch_size, nb_views, nb_cpus=1,

syconn/cnn/cnn_celltype_e3.py
@@ -13,11 +13,12 @@ Caution! The input dataset was not manually corrected.
 from syconn import global_params
 from syconn.cnn.TrainData import CelltypeViewsE3
 import argparse
+import _pickle
 import os
 import torch
 from torch import nn
 from torch import optim
-from elektronn3.models.simple import StackedConv2Scalar
+from elektronn3.models.simple import StackedConv2Scalar, StackedConv2ScalarWithLatentAdd
 from elektronn3.data.transforms import RandomFlip
 from elektronn3.data import transforms
 from elektronn3.training.schedulers import SGDR
@@ -26,18 +27,23 @@ from elektronn3.training import metrics
 
 
 def get_model():
-    model = StackedConv2Scalar(4, 9)
+    model = StackedConv2ScalarWithLatentAdd(in_channels=4, n_classes=9, n_scalar=1)
     return model
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a network.')
     parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
-    parser.add_argument('-n', '--exp-name', default="celltype_e3_axonGTv3_SGDR_run3",
+    parser.add_argument('-n', '--exp-name', default="celltype_e3_axonGTv3_SGDR_LatentAdd_run2",
                         help='Manually set experiment name')
     parser.add_argument(
         '-m', '--max-steps', type=int, default=500000,
         help='Maximum number of training steps to perform.'
+    )
+    parser.add_argument(
+        '-r', '--resume', metavar='PATH',
+        help='Path to pretrained model state dict or a compiled and saved '
+             'ScriptModule from which to resume training.'
     )
     args = parser.parse_args()
     if not args.disable_cuda and torch.cuda.is_available():
@@ -49,7 +55,8 @@ if __name__ == "__main__":
     # Don't move this stuff, it needs to be run this early to work
     import elektronn3
     elektronn3.select_mpl_backend('agg')
-    from elektronn3.training import Trainer, Backup
+    from elektronn3.training import Backup
+    from elektronn3.training.trainer_scalarinput import Trainer
 
     torch.manual_seed(0)
 
@@ -57,13 +64,13 @@ if __name__ == "__main__":
     save_root = os.path.expanduser('~/e3training/')
 
     max_steps = args.max_steps
-    lr = 0.006
+    lr = 0.004
     lr_stepsize = 500
     lr_dec = 0.995
     batch_size = 10
 
     model = get_model()
-    if torch.cuda.device_count() > 1:
+    if 0: #torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         batch_size = batch_size * torch.cuda.device_count()
         # dim = 0 [20, xxx] -> [10, ...], [10, ...] on 2 GPUs
@@ -74,6 +81,14 @@ if __name__ == "__main__":
                         'nb_views_renderinglocations': 4,
                         "reduce_context": 0, "reduce_context_fact": 1, 'ctgt_key': "ctgt_v2", 'random_seed': 0,
                         "binary_views": False, "n_classes": n_classes, 'class_weights': [1] * n_classes}
+
+    if args.resume is not None:  # Load pretrained network
+        try:  # Assume it's a state_dict for the model
+            model.load_state_dict(torch.load(os.path.expanduser(args.resume)))
+        except _pickle.UnpicklingError as exc:
+            # Assume it's a complete saved ScriptModule
+            model = torch.jit.load(os.path.expanduser(args.resume), map_location=device)
+
     # Specify data set
     transform = transforms.Compose([RandomFlip(ndim_spatial=2), ])
     train_dataset = CelltypeViewsE3(train=True, transform=transform, **data_init_kwargs)
@@ -87,8 +102,7 @@ if __name__ == "__main__":
         # amsgrad=True
     )
     # lr_sched = optim.lr_scheduler.StepLR(optimizer, lr_stepsize, lr_dec)
-    lr_sched = optim.lr_scheduler.StepLR(optimizer, lr_stepsize, lr_dec)
-    schedulers = {'lr': SGDR(optimizer, 20000, 1.1)}
+    schedulers = {'lr': SGDR(optimizer, 20000, 2)}
     # All these metrics assume a binary classification problem. If you have
     #  non-binary targets, remember to adapt the metrics!
     val_metric_keys = []

syconn/exec/exec_multiview.py
@@ -196,7 +196,7 @@ def run_spiness_prediction(n_jobs=100):
     multi_params = multi_params[np.argsort(nb_svs_per_ssv)[::-1]]
     multi_params = chunkify(multi_params, 3000)
     # add ssd parameters
-    kwargs_semseg2mesh = dict(semseg_key=pred_key, force_overwrite=True)
+    kwargs_semseg2mesh = dict(semseg_key=pred_key, force_recompute=True)
     multi_params = [(ssv_ids, ssd.version, ssd.version_dict, ssd.working_dir,
                      kwargs_semseg2mesh) for ssv_ids in multi_params]
     log.info('Starting mapping of spine predictions to neurite surfaces.')

syconn/mp/batchjob_utils.py
@@ -480,8 +480,7 @@ def batchjob_fallback(params, name, n_cores=1, suffix="", n_max_co_processes=Non
     if os.path.exists(job_folder):
         shutil.rmtree(job_folder, ignore_errors=True)
     log_batchjob = log_mp
-    if n_max_co_processes is None:
-        n_max_co_processes = cpu_count()
+    n_max_co_processes = np.min([cpu_count(), n_max_co_processes])
     n_max_co_processes = np.min([n_max_co_processes // n_cores, n_max_co_processes])
     n_max_co_processes = np.min([n_max_co_processes, len(params)])
     log_batchjob.debug('Starting BatchJobFallback script "{}" with {} tasks using {}'