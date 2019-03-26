3e87b49b29bd3ebc8513fb18a79b160fff1f23c2
mariakawula <mariakawula2911@gmail.com>
2019-03-26 12:27:16 +0100

    update_before_merging

--- syconn/proc/extract_bounding_box.pyx
+++ syconn/proc/extract_bounding_box.pyx
@@ -1,18 +1,10 @@
 # distutils: language = c++
-from cython.view cimport array as cvarray
-from libc.stdint cimport uint64_t, uint32_t, int
+from libc.stdint cimport uint64_t, uint32_t
 cimport cython
-from libcpp.map cimport map
-from cython.operator import dereference, postincrement
-import timeit
-from libc.stdlib cimport rand
-import numpy as np
-from libcpp.vector cimport vector as cpp_vector
-
 
 ctypedef fused n_type:
     uint64_t
-    uint32_
+    uint32_t
 
 def extract_bounding_box(n_type[:, :, :] chunk):
     box = dict()
@@ -20,33 +12,12 @@ def extract_bounding_box(n_type[:, :, :] chunk):
     for x in range(chunk.shape[0]):
         for y in range(chunk.shape[1]):
             for z in range(chunk.shape[2]):
-
-                
-                box[chunk[x,y,z]].xMin = min(box[chunk[x,y,z]].xMin, x)
-                box[chunk[x,y,z]].xMax = max(box[chunk[x,y,z]].xMax, x)
-
-                box[chunk[x,y,z]].yMin = min(box[chunk[x,y,z]].yMin, y)
-                box[chunk[x,y,z]].yMax = min(box[chunk[x,y,z]].yMax, y)
-
-                box[chunk[x,y,z]].zMin = min(box[chunk[x,y,z]].zMin, z)
-                box[chunk[x,y,z]].zMax = min(box[chunk[x,y,z]].zMax, z)
+                key = chunk[x, y, z]
+                if key in box:
+                    box[key] = {'xMin': min(box[key]['xMin'], x), 'xMax': max(box[key]['xMax'], x),
+                                'yMin': min(box[key]['yMin'], y), 'yMax': max(box[key]['yMax'], y),
+                                'zMin': min(box[key]['zMin'], z), 'zMax': max(box[key]['zMax'], z)}
+                else:
+                    box[key] = {'xMin': x, 'xMax': x, 'yMin': y, 'yMax': y,
+                                'zMin': z, 'zMax': z}
     return box
-
-
-def create_toy_data(int size1, int size2, int size3, int moduloo):
-    np.random.seed(0)
-    cdef uint64_t[:, :, :] matrix = cvarray(shape = (size1, size2, size3),
-                                     itemsize = sizeof(uint64_t), format = 'Q')
-    for i in range(size1):
-        for j in range(size2):
-            matrix[i, j] = np.random.randint(moduloo, size=1)
-    return matrix
-
-
-def main():
-    cdef uint64_t [:, :, :] chunk
-    chunk = create_toy_data(15, 15, 15, 10)
-    extract_bounding_box(chunk)
-    print("done")
-
-main()
