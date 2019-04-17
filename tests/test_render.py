db7f082c25d131d43184eb72bdda9e7f82a4dd02
atul <atulmohite1326@gmail.com>
2019-04-17 14:06:35 +0200

    descending sorting of ssv array, sort_ssv_descending implemented, check test_render.py

--- tests/test_render.py
+++ tests/test_render.py
@@ -18,30 +18,49 @@ class data:
         exloc = np.array([5602, 4173, 4474]) * ssv.scaling
         self.exlocs = np.concatenate(ssv.sample_locations())
 
+
+def sort_ssv_descending(ssv_array):
+    length_array = []
+    index_array = []
+    for ssv_object in ssv_array:
+        length_array.append(len(np.concatenate(ssv_object.sample_locations())))
+    index_array = sorting_in_descending_order(length_array)
+    return index_array
+
+
 def sorting_in_descending_order(array):
     index_array = []
     for i in range(len(array)):
         index_array.append(i)
     for iter_num in range(len(array) - 1, 0, -1):
         for idx in range(iter_num):
-            if list[idx] < list[idx + 1]:
+            if array[idx] < array[idx + 1]:
                 temp = array[idx]
                 array[idx] = array[idx + 1]
                 array[idx + 1] = temp
                 temp = index_array[idx]
                 index_array[idx] = index_array[idx + 1]
                 index_array[idx + 1] = temp
+    print(array)
+    print(index_array)
     return index_array
 
 if __name__=='__main__':
     # TODO: use toy data and improve logging, see test_backend.py
     working_dir = "/wholebrain/scratch/areaxfs3/"
+    location_array = [29753344, 29833344, 30393344, 30493344, 32393344, 34493344, 39553344, 42173344]
+    ssv_array = []
+    length_array = []
     render_indexview = True
     now = time.time()
     ssc = SuperSegmentationDataset(working_dir)
-    ssv = ssc.get_super_segmentation_object(29753344)
-    exlocs = np.concatenate(ssv.sample_locations())
-    exlocs = exlocs[::30]
+    ix = 0
+    for loc in location_array:
+        ssv_array.append(ssc.get_super_segmentation_object(loc))
+        exlocs = np.concatenate(ssv_array[ix].sample_locations())
+        length_array.append(len(exlocs))
+        ix = ix + 1
+    #exlocs = exlocs[::30]
     print("Example location array:", exlocs.shape)
     print(working_dir)
     now2 = time.time()
@@ -50,7 +69,11 @@ if __name__=='__main__':
 
     array = [19,2,31,45,6,11,121,27]
     index_array = sorting_in_descending_order(array)
+    print(array)
     print(index_array)
+    index_array = sort_ssv_descending(ssv_array)
+    print(index_array)
+    print(length_array)
     """
     i = 0
     exlocs = chunkify_successive(exlocs, 10)
@@ -62,7 +85,7 @@ if __name__=='__main__':
         print(params[i-1])
     """
     """
-    view = render_sso_coords_multiprocessing(ssv, working_dir, exlocs, render_indexviews=render_indexview,
+   # view = render_sso_coords_multiprocessing(ssv, working_dir, exlocs, render_indexviews=render_indexview,
                                    n_jobs=20, verbose=True)
 
     now1 = time.time()
