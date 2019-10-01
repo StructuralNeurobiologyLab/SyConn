9cf49ece8780bb002750c208fa6e4b3054c89135
Yang Liu <yliu@wb01.wb.mpcdf.mpg.de>
2019-10-01 13:41:15 +0200

    represent unpredicted label in blue color

--- syconn/reps/super_segmentation_object.py
+++ syconn/reps/super_segmentation_object.py
@@ -2139,9 +2139,9 @@ class SuperSegmentationObject(object):
                                  [0.1, 0.1, 0.9, 1]])
                 cols = (cols * 255).astype(np.uint8)
             elif 'merger_gt' in semseg_key:
-                # cell body, false merger, unpredicted, background
-                cols = np.array([[0.5, 0.5, 0.5, 0.4], [0.96, 0.14, 0.347, 1],
-                                 [0.0, 0.5, 0.0, 0.4], [0.9, 0.9, 0.9, 0.4]])
+                # cell body, false merger, background, unpredicted
+                cols = np.array([[0.5, 0.5, 0.5, 1], [0.96, 0.14, 0.347, 1],
+                                 [0.0, 0.5, 0.0, 1], [0.1, 0.1, 0.9, 1]])
                 cols = (cols * 255).astype(np.uint8)
             else:
                 raise ValueError('Semantic segmentation of "{}" is not supported.'
