687596e87da5ef3d4d233fe08a1b5e0d81f1f0b8
atulm <atultm@neuro.mpg.de>
2019-11-08 17:16:24 +0100

    test script for find_object_properties.py

--- tests/test_segmentation_analysis.py
+++ tests/test_segmentation_analysis.py
@@ -21,31 +21,26 @@ def test_find_objects_properties():
              [2, 1]]],np.uint)
     func_output=find_object_properties(sample_array)
     element, count=np.unique(sample_array,return_counts=True)
-    print(func_output)
-    print(func_output[0])
     for i in range(len(element)):
         if(element[i] != 0 and func_output[2][element[i]] != count[i]):
-            print("Count function not working")
+            assert True, "Count of the voxels not working." \
+                         " They should be same"
         elif i != 0:
             ll = func_output[0][element[i]]
             check = sample_array[ll[0],ll[1],ll[2]]
             if (element[i] != check):
-                print("object voxel dosen't match")
+                assert True, "object voxel directory dosen't match" \
+                             "They should be same"
         # elif(i != 0 and (list(np.unravel_index(first_occurence[i], sample_array.shape)) != func_output[0][element[i]])):
-            # print(list(np.unravel_index(first_occurence[i], sample_array.shape)))
-            # print(func_output[0][element[i]])
         elif():
             continue
-        # print("pp")
     indices= sample_array.flatten()
     min_bound = []
     max_bound = []
-    a=len(indices)
+    a = len(indices)
     for i in range(int(element[len(element)-1]) + 1):
-        min_bound.append([a,a,a])
+        min_bound.append([a, a, a])
         max_bound.append([0, 0, 0])
-    print(min_bound)
-    print(max_bound)
     for i in range(len(indices)):
         # if(element[indices[i]] != 0):
         if (indices[i] != 0):
@@ -53,40 +48,13 @@ def test_find_objects_properties():
             index=list(np.unravel_index(i, sample_array.shape))
             min_bound[qq] = np.minimum(min_bound[qq],index)
             max_bound[qq] = np.maximum(max_bound[qq],index)
-            # print("okay")
     for i in element:
         if i != 0 :
             if not np.array_equal(func_output[1][i][0], min_bound[i]):
                 if not np.array_equal(func_output[1][i][1], (max_bound[i] + [1, 1, 1])):
+                    assert True, "Bounding box directory mismatch." \
+                                 " They should be same"
                     print("Bounding box error")
-    print(min_bound)
-    print(max_bound)
-    # for i in range(len(element)):
-    #
-    #     if(i!=0 and (list(np.unravel_index(first_occurence[i],sample_array.shape))!=func_output[0][element[i]])):
-    #         print(list(np.unravel_index(first_occurence[i], sample_array.shape)))
-    #         print(func_output[0][element[i]])
-    #         print("first occurence don't match")
-    #     else:
-    #         continue
-    # print(func_output)
-    # generate_default_conf(test_dir, scaling=(1, 1, 1), force_overwrite=True)
-    # assert os.path.isfile(test_dir + '/config.yml'), "Error creating config file."
-    # with pytest.raises(ValueError):
-    #     generate_default_conf(test_dir, scaling=(1, 1, 1))
-    # conf = Config(test_dir)
-    # conf2 = Config(test_dir)
-    # assert conf == conf2, "Mismatch between config objects."
-    # entries = conf.entries
-    # entries['scaling'] = (2, 2, 2)
-    # assert conf != conf2, "Expected mismatch between config objects but " \
-    #                       "they are the same."
-    # conf.write_config(test_dir)
-    # conf2 = Config(test_dir)
-    # assert conf == conf2, "Mismatch between config objects after re-loading " \
-    #                       "modified config file."
-    # os.remove(conf.path_config)
-    # logging.info('PASSED: Load and write `Config` class.')
 
 
 if __name__ == '__main__':
