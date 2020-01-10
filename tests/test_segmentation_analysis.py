58c7bf36229f5c030a8e4a48de6084af7b4e67f8
atulm <atultm@neuro.mpg.de>
2020-01-10 14:30:14 +0100

    test case for detect_cs()

--- tests/test_segmentation_analysis.py
+++ tests/test_segmentation_analysis.py
@@ -10,6 +10,42 @@ import scipy.ndimage
 import networkx as nx
 
 
+def test_find_object_properties():
+    sample_array = np.array([
+            [[0, 1],
+             [1, 1]],
+            [[5, 2],
+             [2, 1]]], np.uint64)
+    # func_output[0]=dictionary of id's of unique voxel id, voxel id as key
+    # func_output[1]=dictionary of bounding box for voxel ids, voxel id as key
+    # func_output[2]=dictionary of count of all voxel ids, voxel id as key
+    repcoord_dc, bb_dc, cnt_dc = find_object_properties(sample_array)
+    element, count = np.unique(sample_array, return_counts=True)
+    assert 0 not in repcoord_dc and 0 not in bb_dc and 0 not in cnt_dc, \
+        "Background properties must not be extracted."
+    if 0 in element:
+        count = count[element != 0]
+        element = element[element != 0]
+    for i in range(len(element)):
+        # testing count of voxel ids
+        assert (cnt_dc[element[i]] == count[i].astype(np.uint64)), \
+            "Count of the voxels not working."
+        # testing unique voxel-id
+        ll = repcoord_dc[element[i]]
+        check = sample_array[ll[0], ll[1], ll[2]]
+        assert element[i].astype(np.uint64) == check.astype(np.uint64), \
+            "Object voxel dictionary dosen't match."
+    # testing bounding box
+    for e in element:
+        mask = sample_array == e
+        min_bound = np.transpose(np.where(mask)).min(axis=0)
+        assert np.all(min_bound == bb_dc[e][0]), \
+            "Bounding box dictionary mismatch."
+        max_bound = np.transpose(np.where(mask)).max(axis=0) + 1
+        assert np.all(max_bound == bb_dc[e][1]), \
+            "Bounding box dictionary mismatch."
+
+
 def test_detect_cs(distance_between_cube, stencil, cube_size):
     """
 
@@ -26,7 +62,7 @@ def test_detect_cs(distance_between_cube, stencil, cube_size):
     cube_size = cube_size                                                                #cube size
     distance_between_cube = distance_between_cube                                        #distance between cube
     offset = stencil // 2                                                                #output offset adjustment due to stencil size
-    a = np.amax(offset + 1 )                                                             #co-ordinate of topmost corner of first cube
+    a = np.amax(offset + 1)                                                              #co-ordinate of topmost corner of first cube
     edge_s = np.amax(stencil + distance_between_cube + cube_size)  # data cube size
     sample = np.zeros((edge_s, edge_s, edge_s), dtype=np.uint32)
     c = cube_size
@@ -36,8 +72,8 @@ def test_detect_cs(distance_between_cube, stencil, cube_size):
     edge_id_output_sample = detect_cs(sample)
     higher_id_array = np.asarray(edge_id_output_sample, np.uint32)                       #retracts 32 bit cell id of higher value
     lower_id_array = np.asarray(edge_id_output_sample, np.uint64) // (2 ** 32)           #retracts 32 bit cell id of lower value
-    counter = np.array([c, c, c]) + d + np.array([1, 1, 1])                              #checks if distance between cubes is longer than stencil size
-    output_offset = np.maximum(0, counter - stencil)                                     #adjusts output offset accordingly
+    counter = d - offset                                                                 #checks if distance between cubes is longer than stencil size
+    output_offset = np.maximum(0, counter)                                               #adjusts output offset accordingly
     output_shape = np.array(sample.shape + np.array([1, 1, 1]) - stencil)
     o_o = output_offset                                                                  #dummy variable for output cube size
     o = offset                                                                           #dummy variable for offset due to stencil size
@@ -54,4 +90,6 @@ def test_detect_cs(distance_between_cube, stencil, cube_size):
 
 
 if __name__ == '__main__':
-    test_detect_cs(np.array([0, 5, 0]), np.array(config['cell_objects']['cs_filtersize'], dtype=np.int), 4)
+    test_detect_cs(np.array([0, 6, 0]), np.array(config['cell_objects']['cs_filtersize'], dtype=np.int), 5)
+    test_detect_cs(np.array([6, 0, 0]), np.array(config['cell_objects']['cs_filtersize'], dtype=np.int), 5)
+    test_detect_cs(np.array([0, 0, 6]), np.array(config['cell_objects']['cs_filtersize'], dtype=np.int), 5)
