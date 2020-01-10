62544c20b22af00b8c5b03ec8182bdfb8f75286c
atulm <atultm@neuro.mpg.de>
2020-01-10 13:08:30 +0100

    text case for detect_cs

--- tests/test_segmentation_analysis.py
+++ tests/test_segmentation_analysis.py
@@ -29,57 +29,6 @@ import networkx as nx
 
 
 
-# def test_find_object_properties():
-#     sample_array = np.array([
-#             [[0, 1, 9],
-#              [1, 1, 9],
-#              [0, 1, 9]],
-#             [[5, 2, 4],
-#              [2, 1, 3],
-#              [0, 1, 9]],
-#             [[9, 1, 9],
-#              [2, 3, 4],
-#              [0, 3, 9]]], np.uint64)
-#     func_output = find_object_properties(sample_array)
-#     #func_output[0]=dictionary of id's of unique voxel id, voxel id as key
-#     #func_output[1]=dictionary of bounding box for voxel ids, voxel id as key
-#     #func_output[2]=dictionary of count of all voxel ids, voxel id as key
-#     element, count = np.unique(sample_array, return_counts=True)
-#     for i in range(len(element)):
-#         if element[i] != 0:
-#             #testing count of voxel ids
-#             assert (func_output[2][element[i]] == count[i].astype(np.uint64)), "Count of the voxels not working" \
-#                                                                                " They should be same"
-#             # testing unique voxel-id
-#             ll = func_output[0][element[i]]
-#             check = sample_array[ll[0], ll[1], ll[2]]
-#             assert element[i].astype(np.uint64) == check.astype(np.uint64), "object voxel dictionary dosen't match" \
-#                              "They should be same"
-#     # testing bounding box
-#     indices = sample_array.flatten()
-#     min_bound = np.full(((int(element[len(element)-1]) + 1), 3), len(indices), dtype=np.uint64)
-#     max_bound = np.full(((int(element[len(element)-1]) + 1), 3), 0, dtype=np.uint64)
-#     for i in range(len(indices)):
-#         if indices[i] != 0:
-#             qq = indices[i]
-#             index = list(np.unravel_index(i, sample_array.shape))
-#             min_bound[qq] = np.minimum(np.array(min_bound[qq]), index)
-#             max_bound[qq] = np.maximum(np.array(max_bound[qq]), index)
-#     for i in element:
-#         if i != 0:
-#             assert (np.array_equal(np.array(func_output[1][i][0]), min_bound[i])), \
-#                 "Bounding box dictionary mismatch." \
-#                 " They should be same"
-#             assert np.array_equal(np.array(func_output[1][i][1]), (max_bound[i] + np.ones((3,), dtype=np.uint64))), \
-#                 "Bounding box dictionary mismatch." \
-#                          " They should be same"
-#     print(func_output)
-#     print(min_bound)
-#     print(max_bound)
-#     print(element)
-#     print(count)
-#
-
 def test_detect_cs(distance_between_cube, stencil, cube_size):
     """
 
@@ -91,17 +40,17 @@ def test_detect_cs(distance_between_cube, stencil, cube_size):
     Returns: Assert statement fails if detect_cs() method does not work properly
 
     """
-    edge_s = 20                                                                          #data cube size
-    sample = np.zeros((edge_s, edge_s, edge_s), dtype=np.uint32)
     stencil = stencil
     cube_size = cube_size                                                                #cube size
-    c = cube_size
-    a = 7                                                                                #co-ordinate of topmost corner of first cube
     distance_between_cube = distance_between_cube                                        #distance between cube
+    offset = stencil // 2                                                                #output offset adjustment due to stencil size
+    a = np.amax(offset + 1 )                                                                              #co-ordinate of topmost corner of first cube
+    edge_s = np.amax(stencil + distance_between_cube + cube_size)  # data cube size
+    sample = np.zeros((edge_s, edge_s, edge_s), dtype=np.uint32)
+    c = cube_size
     d = distance_between_cube                                                            #dummy variable
     sample[a:a+c, a:a+c, a:a+c] = 4                                                      #cell_id cube 1
     sample[a+d[0]:a+d[0]+c, a+d[1]:a+d[1]+c, a+d[2]:a+d[2]+c] = 5                        #cell_id cube 2
-    offset = stencil // 2                                                                #output offset adjustment due to stencil size
     edge_id_output_sample = detect_cs(sample)
     higher_id_array = np.asarray(edge_id_output_sample, np.uint32)                       #retracts 32 bit cell id of higher value
     lower_id_array = np.asarray(edge_id_output_sample, np.uint64) // (2 ** 32)           #retracts 32 bit cell id of lower value
@@ -121,17 +70,6 @@ def test_detect_cs(distance_between_cube, stencil, cube_size):
     assert np.array_equal(np.array(5*output_id, np.uint32), np.array(higher_id_array, np.uint32)), "higher value cell id array do not match"
     assert np.array_equal(np.array(4*output_id, np.uint32), np.array(lower_id_array, np.uint32)), "lower value cell id array do not match"
 
-    # print(5*output_id)
-    # print(higher_id_array)
-    # print(output_shape)
-    # print(output_offset)
-    # sample[7:10, 7:10, 7:10] = 4                                                         #cell_id cube 1
-    # sample[7:10, 7:10, 11:14] = 5                                                        #cell_id cube 2
-
-
-def test_process_block_nonzero():
-    print("p")
-
 
 if __name__ == '__main__':
-    test_detect_cs(np.array([0, 4, 4]), np.array(config['cell_objects']['cs_filtersize'], dtype=np.int), 3)
+    test_detect_cs(np.array([4, 0, 0]), np.array(config['cell_objects']['cs_filtersize'], dtype=np.int), 3)
