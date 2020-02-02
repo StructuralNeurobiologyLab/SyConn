0c9cbc7a76315a6c0875c3d749c3c7396a2d55bd
Philipp <p.schubert@stud.uni-heidelberg.de>
2020-02-02 11:57:15 +0100

    switched to new h5py interface: changed `.value` to `[()]` in `load_from_h5py`

--- syconn/handler/basics.py
+++ syconn/handler/basics.py
@@ -61,9 +61,9 @@ def load_from_h5py(path, hdf5_names=None, as_dict=False):
             hdf5_names = f.keys()
         for hdf5_name in hdf5_names:
             if as_dict:
-                data[hdf5_name] = f[hdf5_name].value
+                data[hdf5_name] = f[hdf5_name][()]
             else:
-                data.append(f[hdf5_name].value)
+                data.append(f[hdf5_name][()])
     except:
         raise Exception("Error at Path: %s, with labels:" % path, hdf5_names)
     f.close()
