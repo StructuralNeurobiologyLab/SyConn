a840fe7b841893a7d6e2cf26f1f24d5ab81f2c89
PhylomatX <klimesch.jonathan@gmail.com>
2020-06-07 17:39:43 +0200

    Minor changes

--- scripts/point_party/infer_cpmt.py
+++ scripts/point_party/infer_cpmt.py
@@ -1,23 +1,26 @@
 import numpy as np
-from syconn.handler.prediction_pts import predict_cpmt_ssv
+import morphx.processing.clouds as clouds
+from elektronn3.models.convpoint import SegBig
+from syconn.handler.prediction_pts import predict_cpmt_ssv, pts_loader_cpmt, pts_pred_cmpt
 from syconn.reps.super_segmentation import SuperSegmentationDataset
 
 
-if __name__ == '__main__':
+def test_cmpt_pipeline():
     wd = "/wholebrain/songbird/j0126/areaxfs_v6/"
-    mpath = ''
-
     ssd_kwargs = dict(working_dir=wd)
     ssd = SuperSegmentationDataset(**ssd_kwargs)
-
-    # ixs = np.random.choice(len(ssd.ssv_ids), 10, replace=False)
-    # ssv_ids = ssd.ssv_ids[ixs]
-    # sizes = ssd.load_cached_data('size')[ixs]
-    # ssv_ids = ssv_ids[np.argsort(sizes)[::-1]]
-
     ssv_ids = np.array([2734465, 2854913, 8003584, 8339462, 10919937, 26169344, 26501121])
     ssv_params = [dict(ssv_id=ssv.id, sv_ids=ssv.sv_ids, sv_graph=ssv.sv_graph_uint,
                        **ssd_kwargs) for ssv in ssd.get_super_segmentation_object(ssv_ids)]
+    m = SegBig(4, 3)
+    transform = [clouds.Normalization(5000), clouds.Center()]
+    transform = clouds.Compose(transform)
 
-    res = predict_cpmt_ssv(ssv_params, mpath=mpath, nloader=1, npredictor=1,
-                           postproc_kwargs=dict(pred_key='cpmt_probas_test'))
+    # pipeline simulation
+    import ipdb
+    ipdb.set_trace()
+    res = pts_loader_cpmt([ssv_params[0]], 8, 2000, 5000, use_myelin=False, transform=transform, base_node_dst=10000)
+
+
+if __name__ == '__main__':
+    test_cmpt_pipeline()
