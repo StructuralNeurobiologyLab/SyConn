666e8a3d941ba53a0c019d6845e4c45268e92946
PhylomatX <klimesch.jonathan@gmail.com>
2020-08-25 14:59:54 +0200

    Added script for direct inference of SuperSegmentationObjects.

--- jklimesch/scripts/explore_syconn.py
+++ jklimesch/scripts/explore_syconn.py
@@ -1,7 +1,9 @@
 import os
 import numpy as np
-from typing import List, Optional
+import open3d as o3d
+from typing import List, Optional, Dict
 from morphx.classes.hybridcloud import HybridCloud
+from morphx.classes.cloudensemble import CloudEnsemble
 from morphx.processing.clouds import Center
 from syconn.reps.super_segmentation import SuperSegmentationDataset
 
@@ -34,10 +36,44 @@ def sso2hc(sso_ids: List[int], data: SuperSegmentationDataset, save_path: str, l
                 hc.save2pkl(f'{save_path}{sso_id}_{key}.pkl')
 
 
+def sso2pickle(sso_ids: List[int], data: SuperSegmentationDataset, save_path: str, cos: List[str],
+               voxel_dc: Dict[str, int]):
+    save_path = os.path.expanduser(save_path)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    for sso_id in sso_ids:
+        if sso_id not in data.ssv_ids:
+            continue
+        sso = data.get_super_segmentation_object(sso_id)
+        clouds = {}
+        ce_hc = None
+        no_pred = []
+        for co in cos:
+            pcd = o3d.geometry.PointCloud()
+            verts = sso.load_mesh(co)[1].reshape(-1, 3)
+            pcd.points = o3d.utility.Vector3dVector(verts)
+            pcd, _ = pcd.voxel_down_sample_and_trace(voxel_dc[co], pcd.get_min_bound(), pcd.get_max_bound())
+            verts = np.asarray(pcd.points)
+            if co == 'sv':
+                sso.load_skeleton()
+                nodes, edges = sso.skeleton['nodes'] * sso.scaling, sso.skeleton['edges']
+                hc = HybridCloud(vertices=verts, nodes=nodes, edges=edges)
+                ce_hc = hc
+            else:
+                hc = HybridCloud(vertices=verts)
+                clouds[co] = hc
+                no_pred.append(co)
+        ce = CloudEnsemble(clouds, ce_hc, no_pred=no_pred)
+        ce.save2pkl(f'{save_path}/sso_{sso.id}.pkl')
+
+
 if __name__ == '__main__':
-    ssd = SuperSegmentationDataset(
-        working_dir="/wholebrain/scratch/arother/assembled_core_relabeled_base_merges_relabeled_to_v4b_base_20180214_full_agglo_cbsplit_with_reconnects_no_soma_merger_manual_edges_removed/")
-    path = '~/thesis/current_work/explore_syconn/kimimaro/'
-    ids = [25362218,   107283,  9221702,  1619851, 12231815, 34217175, 29327350, 25393993,  6993099, 22802417,
-           2584916, 17920291, 16902380,  2169440, 32327585,  2218026, 24556061, 26148645, 29840046,  1226094]
-    sso2hc(ids, ssd, path)
+    # ssd = SuperSegmentationDataset(working_dir="/wholebrain/scratch/areaxfs3/")
+    # path = '~/thesis/gt/syn_gt/'
+    # ids = [141995, 11833344, 28410880, 28479489]
+    # sso2pickle(ids, ssd, path, ['sv', 'mi', 'vc', 'sj'], voxel_dc={'sv': 80, 'mi': 100, 'vc': 100, 'sj': 100})
+
+    ssd = SuperSegmentationDataset(working_dir="/wholebrain/scratch/areaxfs3/")
+    path = '~/thesis/current_work/explore_syconn/inference_test/'
+    ids = [141995]
+    sso2hc(ids, ssd, path, label_keys=['ds', 'ads'])
---
+++ jklimesch/scripts/inference.py
@@ -0,0 +1,181 @@
+import os
+import numpy as np
+import open3d as o3d
+import torch
+import math
+import time
+from tqdm import tqdm
+from typing import List, Tuple
+from collections import defaultdict
+from morphx.preprocessing import splitting
+from morphx.classes.hybridcloud import HybridCloud
+from morphx.processing import objects, clouds
+from morphx.classes.pointcloud import PointCloud
+from sklearn.preprocessing import label_binarize
+from elektronn3.models.convpoint import SegBig, SegAdapt
+from neuronx.classes.argscontainer import ArgsContainer
+from syconn.reps.super_segmentation_dataset import SuperSegmentationDataset
+
+
+def predict_sso(sso_ids: List[int], ssd: SuperSegmentationDataset, model_p: str, model_args_p: str, pred_key: str):
+    model_p = os.path.expanduser(model_p)
+    model_args_p = os.path.expanduser(model_args_p)
+
+    argscont = ArgsContainer().load_from_pkl(model_args_p)
+
+    if argscont.use_cuda:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    # load model
+    if argscont.use_big:
+        model = SegBig(argscont.input_channels, argscont.class_num, trs=argscont.track_running_stats, dropout=0,
+                       use_bias=argscont.use_bias, norm_type=argscont.norm_type, use_norm=argscont.use_norm,
+                       kernel_size=argscont.kernel_size, neighbor_nums=argscont.neighbor_nums,
+                       reductions=argscont.reductions, first_layer=argscont.first_layer,
+                       padding=argscont.padding, nn_center=argscont.nn_center, centroids=argscont.centroids,
+                       pl=argscont.pl, normalize=argscont.cp_norm)
+    else:
+        model = SegAdapt(argscont.input_channels, argscont.class_num, architecture=argscont.architecture,
+                         trs=argscont.track_running_stats, dropout=argscont.dropout, use_bias=argscont.use_bias,
+                         norm_type=argscont.norm_type, kernel_size=argscont.kernel_size, padding=argscont.padding,
+                         nn_center=argscont.nn_center, centroids=argscont.centroids, kernel_num=argscont.pl,
+                         normalize=argscont.cp_norm, act=argscont.act)
+    try:
+        full = torch.load(model_p)
+        model.load_state_dict(full)
+    except RuntimeError:
+        model.load_state_dict(full['model_state_dict'])
+    model.to(device)
+    model.eval()
+
+    voxel_dc = {'sv': 80, 'mi': 100, 'vc': 100, 'sy': 100}
+    feats = argscont.features
+    if 'hc' in feats:
+        feats['sv'] = feats['hc']
+        feats.pop('hc')
+    parts = {}
+    for key in feats:
+        parts[key] = (voxel_dc[key], feats[key])
+
+    for sso_id in tqdm(sso_ids):
+        sso = ssd.get_super_segmentation_object(sso_id)
+        vert_dc = {}
+        voxel_idcs = {}
+        offset = 0
+        obj_bounds = {}
+        for ix, k in enumerate(parts):
+            pcd = o3d.geometry.PointCloud()
+            verts = sso.load_mesh(k)[1].reshape(-1, 3)
+            pcd.points = o3d.utility.Vector3dVector(verts)
+            pcd, idcs = pcd.voxel_down_sample_and_trace(parts[k][0], pcd.get_min_bound(), pcd.get_max_bound())
+            voxel_idcs[k] = np.max(idcs, axis=1)
+            vert_dc[k] = np.asarray(pcd.points)
+            obj_bounds[k] = [offset, offset + len(pcd.points)]
+            offset += len(pcd.points)
+
+        if type(parts['sv'][1]) == int:
+            sample_feats = np.concatenate([[parts[k][1]] * len(vert_dc[k]) for k in parts]).reshape(-1, 1)
+            sample_feats = label_binarize(sample_feats, classes=np.arange(len(parts)))
+        else:
+            feats_dc = {}
+            for key in parts:
+                sample_feats = np.ones((len(vert_dc[key]), len(parts[key][1])))
+                sample_feats[:] = parts[key][1]
+                feats_dc[key] = sample_feats
+            sample_feats = np.concatenate([feats_dc[k] for k in parts])
+
+        sample_pts = np.concatenate([vert_dc[k] for k in parts])
+        if not sso.load_skeleton():
+            raise ValueError(f'Couldnt find skeleton of {sso}')
+        nodes, edges = sso.skeleton['nodes'] * sso.scaling, sso.skeleton['edges']
+        hc = HybridCloud(nodes, edges, vertices=sample_pts, features=sample_feats, obj_bounds=obj_bounds)
+        node_arrs = splitting.split_single(hc, argscont.chunk_size, argscont.chunk_size / 20)
+
+        transform = clouds.Compose(argscont.val_transforms)
+        samples = []
+        for node_arr in tqdm(node_arrs):
+            sample, idcs_sub = objects.extract_cloud_subset(hc, node_arr)
+            sample, idcs_sample = clouds.sample_cloud(sample, argscont.sample_num, padding=argscont.padding)
+            idcs_global = idcs_sub[idcs_sample.astype(int)]
+            bounds = hc.obj_bounds['sv']
+            sv_mask = np.logical_and(idcs_global < bounds[1], idcs_global >= bounds[0])
+            idcs_global[np.logical_not(sv_mask)] = -1
+            if len(sample.vertices) == 0:
+                continue
+            transform(sample)
+            samples.append((sample, idcs_global))
+
+        preds = []
+        idcs_preds = []
+        with torch.no_grad():
+            batches = batch_builder(samples, argscont.batch_size, argscont.input_channels)
+            for batch in batches:
+                pts = batch[0].to(device, non_blocking=True)
+                features = batch[1].to(device, non_blocking=True)
+                outputs = model(features, pts)
+                outputs = outputs.cpu().detach().numpy()
+                for ix in range(argscont.batch_size):
+                    preds.append(np.argmax(outputs[ix], axis=1))
+                    idcs_preds.append(batch[2][ix])
+
+        preds = np.concatenate(preds)
+        idcs_preds = np.concatenate(idcs_preds)
+        # filter possible organelles
+        preds = preds[idcs_preds != -1]
+        idcs_preds = idcs_preds[idcs_preds != -1].astype(int)
+        pred_labels = np.ones((len(voxel_idcs['sv']), 1))*-1
+        print("Evaluating predictions...")
+        start = time.time()
+        evaluate_preds(idcs_preds, preds, pred_labels)
+        print(f"Finished evaluation in {time.time()-start} seconds.")
+        sso_vertices = sso.mesh[1].reshape((-1, 3))
+        sso_preds = np.ones((len(sso_vertices), 1)) * -1
+        sso_preds[voxel_idcs['sv']] = pred_labels
+        ld = sso.label_dict('vertex')
+        ld[pred_key] = sso_preds
+        ld.push()
+
+
+def evaluate_preds(preds_idcs: np.ndarray, preds: np.ndarray, pred_labels: np.ndarray):
+    """ ith entry in ``preds_idcs`` contains vertex index of prediction saved at ith entry of preds.
+        Predictions for each vertex index are gathered and then evaluated by a majority vote.
+        The result gets saved at the respective index in the pred_labels array. """
+    pred_dict = defaultdict(list)
+    u_preds_idcs = np.unique(preds_idcs)
+    for i in range(len(preds_idcs)):
+        pred_dict[preds_idcs[i]].append(preds[i])
+    for u_ix in u_preds_idcs:
+        counts = np.bincount(pred_dict[u_ix])
+        pred_labels[u_ix] = np.argmax(counts)
+
+
+def batch_builder(samples: List[Tuple[PointCloud, np.ndarray]], batch_size: int, input_channels: int):
+    point_num = len(samples[0][0].vertices)
+    batch_num = math.ceil(len(samples)/batch_size)
+    batches = []
+    ix = -1
+    for batch_ix in range(batch_num):
+        pts = torch.zeros((batch_size, point_num, 3))
+        features = torch.ones((batch_size, point_num, input_channels))
+        mapping_idcs = torch.ones((batch_size, point_num))
+        for sample_ix in range(batch_size):
+            ix += 1
+            if ix == len(samples):
+                ix = 0
+            pts[sample_ix] = torch.from_numpy(samples[ix][0].vertices).float()
+            features[sample_ix] = torch.from_numpy(samples[ix][0].features).float()
+            mapping_idcs[sample_ix] = torch.from_numpy(samples[ix][1])
+        batches.append((pts, features, mapping_idcs))
+    return batches
+
+
+if __name__ == '__main__':
+    m_path = '~/thesis/current_work/paper/dnh/2020_08_18_4000_4000_spgt_ds_no_co_randelastic_scale/' \
+             'models/state_dict_e450.pth'
+    argscont_path = '~/thesis/current_work/paper/dnh/2020_08_18_4000_4000_spgt_ds_no_co_randelastic_scale/argscont.pkl'
+    predict_sso([141995, 11833344, 28410880, 28479489],
+                SuperSegmentationDataset(working_dir="/wholebrain/scratch/areaxfs3/"),
+                m_path, argscont_path, 'ds_highred20')
+
