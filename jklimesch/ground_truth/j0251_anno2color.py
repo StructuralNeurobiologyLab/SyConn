514b888a8e8d866717980e08a2d9f9f20ad14ac7
PhylomatX <klimesch.jonathan@gmail.com>
2021-03-13 11:39:04 +0100

    Updated anno2color script to save new versions of the kzips instead of copying the old.

--- jklimesch/ground_truth/j0251_anno2color.py
+++ jklimesch/ground_truth/j0251_anno2color.py
@@ -4,9 +4,8 @@ import os
 import re
 from tqdm import tqdm
 from syconn.proc.meshes import write_mesh2kzip
-from morphx.classes.hybridmesh import HybridMesh
 from morphx.classes.hybridcloud import HybridCloud
-from utils import anno_skeleton2np
+from utils import anno_skeleton2np, comment2unique, unique2comment, sso2kzip, nxGraph2kzip
 from syconn.reps.super_segmentation import SuperSegmentationDataset
 
 
@@ -16,43 +15,38 @@ col_lookup = {0: (76, 92, 158, 255), 1: (255, 125, 125, 255), 2: (125, 255, 125,
               12: (255, 127, 15, 255)}
 
 
-def kzip2pkl(sso_id, ssd, kzip):
-    ssd = SuperSegmentationDataset(working_dir=ssd)
+def process_file(file: str, ctype: str, ssd: SuperSegmentationDataset):
+    sso_id = int(max(re.findall('(\d+)', file.replace(a_path, '')), key=len))
     sso = ssd.get_super_segmentation_object(sso_id)
-
     scaling = sso.scaling
-    if 'DA_' in kzip or 'HVC_' in kzip or '6000389' in kzip or '1358090' in kzip or '10074977' in kzip:
+    if 'DA_' in file or 'HVC_' in file or '6000389' in file or '1358090' in file or '10074977' in file:
         scaling = np.array([10, 10, 20])
-    if 'HVC_53854647' in kzip:
+    if 'HVC_53854647' in file:
         scaling = sso.scaling
-    a_coords, a_edges, a_labels = anno_skeleton2np(kzip, scaling, verbose=True)
+    a_coords, a_edges, a_labels, a_labels_raw, graph = anno_skeleton2np(file, scaling, verbose=True)
 
     indices, vertices, normals = sso.mesh
     vertices = vertices.reshape((-1, 3))
     labels = np.ones((len(vertices), 1)) * -1
     indices = indices.reshape((-1, 3))
-    hc = HybridCloud(vertices=vertices, labels=labels, nodes=a_coords, edges=a_edges,
-                     node_labels=a_labels)
+    hc = HybridCloud(vertices=vertices, labels=labels, nodes=a_coords, edges=a_edges, node_labels=a_labels)
     hc.nodel2vertl()
-    return hc, indices
 
-
-def process_file(file: str, ctype: str):
-    sso_id = int(max(re.findall('(\d+)', file.replace(a_path, '')), key=len))
-    hc, faces = kzip2pkl(sso_id, ssd, file)
-    os.rename(file, o_path + ctype + f'_{sso_id}.k.zip')
+    output_path = o_path + ('DA' if ctype == 'DA_' else ctype) + f'_{sso_id}.k.zip'
+    sso2kzip(sso_id, ssd, output_path, skeleton=False)
     cols = np.array([col_lookup[el] for el in hc.labels.squeeze()], dtype=np.uint8)
-    write_mesh2kzip(f'{o_path}{ctype}_{sso_id}.k.zip', faces.astype(np.float32),
-                    hc.vertices.astype(np.float32), None, cols, f'colored.ply')
-    hc.save2pkl(o_path + ctype + f'_{sso_id}.pkl')
+    write_mesh2kzip(output_path, indices.astype(np.float32), hc.vertices.astype(np.float32), None, cols, f'colored.ply')
+    labels = [unique2comment(int(label)) for label in a_labels_raw]
+    nxGraph2kzip(graph, a_coords, labels, output_path)
+    # hc.save2pkl(o_path + ('DA' if ctype == 'DA_' else ctype) + f'_{sso_id}.pkl')
 
 
 if __name__ == '__main__':
-    a_path = '/wholebrain/u/jklimesch/working_dir/gt/j0251/21_02_02_annotations/raw/'
-    o_path = '/wholebrain/u/jklimesch/working_dir/gt/j0251/21_02_02_annotations/colored/'
+    a_path = '/wholebrain/u/jklimesch/working_dir/gt/j0251/21_03_13_annotations_refinment_round1/raw/'
+    o_path = '/wholebrain/u/jklimesch/working_dir/gt/j0251/21_03_13_annotations_refinment_round1/colored/'
     if not os.path.exists(o_path):
         os.makedirs(o_path)
-    ssd = '/ssdscratch/pschuber/songbird/j0251/rag_flat_Jan2019_v2/'
+    ssd = SuperSegmentationDataset(working_dir='/ssdscratch/pschuber/songbird/j0251/rag_flat_Jan2019_v2/')
     files = os.listdir(a_path)
     for file in files:
         print(f'Processing: {file}')
@@ -60,7 +54,7 @@ if __name__ == '__main__':
             kzips = glob.glob(a_path + file + '/*k.zip')
             for kzip in tqdm(kzips):
                 print(f'Processing: {kzip}')
-                process_file(kzip, file)
+                process_file(kzip, file, ssd)
         else:
-            process_file(a_path + file, file[:3])
+            process_file(a_path + file, file[:3], ssd)
 
--- jklimesch/ground_truth/utils.py
+++ jklimesch/ground_truth/utils.py
@@ -1,8 +1,11 @@
-import numpy as np
-from knossos_utils.skeleton_utils import load_skeleton
-import networkx as nx
+from syconn.reps.super_segmentation import SuperSegmentationDataset
 from tqdm import tqdm
+import math
+import numpy as np
+import networkx as nx
 from collections import deque
+from knossos_utils.skeleton_utils import load_skeleton
+from knossos_utils.skeleton import Skeleton, SkeletonAnnotation, SkeletonNode
 
 
 def label_search(g: nx.Graph, source: int) -> int:
@@ -21,43 +24,20 @@ def label_search(g: nx.Graph, source: int) -> int:
     return -1
 
 
-def anno_skeleton2np(kzip, scaling, verbose=False):
-    a_obj = load_skeleton(kzip)
-    a_obj = dict(skeleton=a_obj['skeleton'])
-    a_obj = list(a_obj.values())[0]
-    a_nodes = list(a_obj.getNodes())
-    a_node_coords = np.array([n.getCoordinate() * scaling for n in a_nodes])
-    a_node_labels = np.array([comment2int(n.getComment()) for n in a_nodes], dtype=np.int)
-    # generate graph from nodes in annotation object
-    a_edges = []
-    if verbose:
-        print("Building graph...")
-    for node in tqdm(a_nodes, disable=not verbose):
-        ix = a_nodes.index(node)
-        neighbors = node.getNeighbors()
-        for neighbor in neighbors:
-            nix = a_nodes.index(neighbor)
-            a_edges.append((ix, nix))
-    g = nx.Graph()
-    g.add_nodes_from([(i, dict(label=a_node_labels[i])) for i in range(len(a_nodes))])
-    g.add_edges_from(a_edges)
-    a_edges = np.array(g.edges)
-    # propagate labels, nodes with no label get label from nearest node with label
-    if verbose:
-        print("Propagating labels...")
-    if -1 in a_node_labels:
-        nodes = np.array(g.nodes)
-        # shuffling to make sure the label searches are not done for consecutive nodes => brings potential speedup
-        np.random.shuffle(nodes)
-        for node in tqdm(nodes, disable=not verbose):
-            if a_node_labels[node] == -1:
-                ix = label_search(g, node)
-                if ix == -1:
-                    a_node_labels[node] = 11
-                else:
-                    path = nx.shortest_path(g, node, ix)
-                    a_node_labels[path] = a_node_labels[ix]
-    return a_node_coords, a_edges, a_node_labels
+def nxGraph2kzip(g, coords, labels, kzip_path):
+    skel = Skeleton()
+    anno = SkeletonAnnotation()
+    node_mapping = {}
+    for v in g.nodes():
+        c = coords[v]
+        n = SkeletonNode().from_scratch(anno, c[0], c[1], c[2])
+        n.setComment(labels[v])
+        node_mapping[v] = n
+        anno.addNode(n)
+    for e in g.edges():
+        anno.addEdge(node_mapping[e[0]], node_mapping[e[1]])
+    skel.add_annotation(anno)
+    skel.to_kzip(kzip_path)
 
 
 def comment2int(comment: str):
@@ -91,3 +71,60 @@ def comment2int(comment: str):
         return 12
     else:
         return -1
+
+
+def comment2unique(s):
+    return int.from_bytes(s.encode(), 'little')
+
+
+def unique2comment(n):
+    return n.to_bytes(math.ceil(n.bit_length() / 8), 'little').decode()
+
+
+def sso2kzip(id: int, ssd: SuperSegmentationDataset, ouput_path: str, skeleton: bool = True):
+    sso = ssd.get_super_segmentation_object(id)
+    sso.load_attr_dict()
+    sso.meshes2kzip(ouput_path, synssv_instead_sj=True)
+    if skeleton:
+        sso.load_skeleton()
+        sso.save_skeleton_to_kzip(ouput_path)
+
+
+def anno_skeleton2np(kzip, scaling, verbose=False):
+    a_obj = load_skeleton(kzip)
+    a_obj = dict(skeleton=a_obj['skeleton'])
+    a_obj = list(a_obj.values())[0]
+    a_nodes = list(a_obj.getNodes())
+    a_node_coords = np.array([n.getCoordinate() * scaling for n in a_nodes])
+    a_node_labels = np.array([comment2int(n.getComment()) for n in a_nodes], dtype=np.int)
+    a_node_labels_raw = np.array([comment2unique(n.getComment()) for n in a_nodes], dtype=np.int)
+    # generate graph from nodes in annotation object
+    a_edges = []
+    if verbose:
+        print("Building graph...")
+    for node in tqdm(a_nodes, disable=not verbose):
+        ix = a_nodes.index(node)
+        neighbors = node.getNeighbors()
+        for neighbor in neighbors:
+            nix = a_nodes.index(neighbor)
+            a_edges.append((ix, nix))
+    g = nx.Graph()
+    g.add_nodes_from([(i, dict(label=a_node_labels[i])) for i in range(len(a_nodes))])
+    g.add_edges_from(a_edges)
+    a_edges = np.array(g.edges)
+    # propagate labels, nodes with no label get label from nearest node with label
+    if verbose:
+        print("Propagating labels...")
+    if -1 in a_node_labels:
+        nodes = np.array(g.nodes)
+        # shuffling to make sure the label searches are not done for consecutive nodes => brings potential speedup
+        np.random.shuffle(nodes)
+        for node in tqdm(nodes, disable=not verbose):
+            if a_node_labels[node] == -1:
+                ix = label_search(g, node)
+                if ix == -1:
+                    a_node_labels[node] = 11
+                else:
+                    path = nx.shortest_path(g, node, ix)
+                    a_node_labels[path] = a_node_labels[ix]
+    return a_node_coords, a_edges, a_node_labels, a_node_labels_raw, g
