46adfcb6af461172adf0eb5412c29bf15e282e79
Philipp Schubert <phil.jo.schubert@gmail.com>
2021-11-29 14:36:30 +0100

    hotfix: apply masks correctly in `filter_relevant_syn` in the case of 'cs'

--- syconn/extraction/cs_processing_steps.py
+++ syconn/extraction/cs_processing_steps.py
@@ -295,16 +295,19 @@ def filter_relevant_syn(sd_syn: segmentation.SegmentationDataset,
     if (sd_syn.type == 'cs') and (min_path_length_partners is not None) and (min_path_length_partners > 0):
         filtered_ssv_ids = filter_ssd_by_total_pathlength(ssd, min_path_length_partners)
         log.info(f'Filtering contact sites formed with at least once small cell (min. path length of a '
-                            f'cell {min_path_length_partners} µm). {len(filtered_ssv_ids)} Cells fulfill that '
-                            f'criterion.')
+                 f'cell {min_path_length_partners} µm). {len(filtered_ssv_ids)} Cells fulfill that '
+                 f'criterion.')
         # check for every element of inter_ssv_contacts if it is inside filtered_ssv_ids
         res = np.isin(inter_ssv_contacts.reshape(-1), filtered_ssv_ids).reshape(-1, 2)
-        inter_ssv_contacts = inter_ssv_contacts[np.all(res, axis=1)]
+        mask = np.all(res, axis=1)
+        inter_ssv_contacts = inter_ssv_contacts[mask]
+        syn_ids = syn_ids[mask]
+        assert len(inter_ssv_contacts) == len(syn_ids)
         if len(inter_ssv_contacts) == 0:
             log.warning(f'No contact site found after filtering small cells.')
         else:
             log.info(f'Found {len(inter_ssv_contacts)} supervoxel contact sites (merged, unsplit) between'
-                                f' {len(filtered_ssv_ids)} cells.')
+                     f' {len(filtered_ssv_ids)} cells.')
     # get bit shifted combination of SSV partner IDs, used to collect all corresponding synapse IDs between the two
     # cells
     relevant_ssv_ids_enc = np.left_shift(np.max(inter_ssv_contacts, axis=1), 32) + np.min(inter_ssv_contacts, axis=1)
@@ -727,7 +730,8 @@ def _combine_and_split_cs_thread(args):
             csssv_attr_dc["mesh_area"] = cs_ssv.mesh_area
             csssv_attr_dc["bounding_box"] = cs_ssv.mesh_bb // scaling
             csssv_attr_dc["rep_coord"] = seghelp.calc_center_of_mass(mesh_cc[1].reshape((-1, 3))) // scaling
-
+            # TODO: PS remove
+            raise()
             # create open3d mesh instance to compute volume
             # # TODO: add this as soon open3d >= 0.11 is supported (glibc error on cluster prevents upgrade)
             # tm = o3d.geometry.TriangleMesh
