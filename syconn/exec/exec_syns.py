7094508d190f2bb5b398fe04739ac38c578f29f2
arother <rother@neuro.mpg.de>
2023-05-24 14:47:09 +0200

    adjust memory for spinehead volume calculation

--- syconn/exec/exec_syns.py
+++ syconn/exec/exec_syns.py
@@ -91,7 +91,7 @@ def run_syn_generation(chunk_size: Optional[Tuple[int, int, int]] = (512, 512, 5
     """
     log = initialize_logging('synapse_detection', global_params.config.working_dir + '/logs/',
                              overwrite=True)
-    '''
+
     kd_seg_path = global_params.config.kd_seg_path
     kd = kd_factory(kd_seg_path)
     if cube_of_interest_bb is None:
@@ -130,12 +130,12 @@ def run_syn_generation(chunk_size: Optional[Tuple[int, int, int]] = (512, 512, 5
     cps.combine_and_split_syn(global_params.config.working_dir,
                               cs_gap_nm=global_params.config['cell_objects']['cs_gap_nm'],
                               log=log, n_folders_fs=n_folders_fs, overwrite=overwrite, exclude_nodes = exclude_nodes)
-    '''
+
     sd_syn_ssv = SegmentationDataset(working_dir=global_params.config.working_dir,
                                      obj_type='syn_ssv')
     # recompute=False: size, bounding box, rep_coord and mesh properties
     # have already been processed in combine_and_split_syn
-    '''
+
     dataset_analysis(sd_syn_ssv, compute_meshprops=False, recompute=False)
     syn_sign = sd_syn_ssv.load_numpy_data('syn_sign')
     n_sym = np.sum(syn_sign == -1)
@@ -147,7 +147,7 @@ def run_syn_generation(chunk_size: Optional[Tuple[int, int, int]] = (512, 512, 5
              f'objects, {n_sym} symmetric, {n_asym} asymmetric and '
              f'{(len(sd_syn_ssv.ids) / np.prod(dataset_vol) * 1e9):0.4f} synapses / µm^3.')
     assert n_sym + n_asym == len(sd_syn_ssv.ids)
-    '''
+
 
     cps.map_objects_from_synssv_partners(global_params.config.working_dir, log=log)
 
@@ -203,7 +203,7 @@ def run_spinehead_volume_calc():
     performed before run_matrix_export.
 
     """
-    log = initialize_logging('compartment_prediction', global_params.config.working_dir + '/logs/',
+    log = initialize_logging('run spinehead calc', global_params.config.working_dir + '/logs/',
                              overwrite=False)
     ssd = SuperSegmentationDataset(working_dir=global_params.config.working_dir)
     log.info('Starting spine head volume calculation.')
@@ -211,5 +211,5 @@ def run_spinehead_volume_calc():
     multi_params = chunkify(multi_params, global_params.config.ncore_total * 4)
     multi_params = [(ixs,) for ixs in multi_params]
 
-    batchjob_script(multi_params, "calculate_spinehead_volume", log=log, remove_jobfolder=True)
+    batchjob_script(multi_params, "calculate_spinehead_volume", log=log, remove_jobfolder=False, additional_flags='--mem=12000')
     log.info(f'Finished processing of {len(ssd.ssv_ids)} SSVs.')
