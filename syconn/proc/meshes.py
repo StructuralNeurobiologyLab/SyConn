3a81e650119630d56b1b2aa675ebea9569b7f6f0
arother <rother@neuro.mpg.de>
2023-08-08 17:04:01 +0200

    add description to compartmentalize_mesh_fromskel

--- syconn/proc/meshes.py
+++ syconn/proc/meshes.py
@@ -867,7 +867,7 @@ def compartmentalize_mesh_fromskel(ssv: 'super_segmentation_object.SuperSegmenta
     """
     Based on compartmentalize_mesh but uses skeleton coordinates and axoness prediction. Splits SuperSegmentationObject mesh into axon, dendrite and soma. Based
     on axoness prediction of SV's contained in SuperSuperVoxel ssv. Skeleton of cell needs to be loaded.
-
+    If not enough soma skeleton nodes or only ones far away from surface, might not find the corresponding vertices.
     Args:
         ssv: SuperSegmentationObject
         pred_key_appendix: str
